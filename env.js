0faf722a61011b8dec3441273afd81e5567664f3
Jose Jollja <jose.jollja@unmsm.edu.pe>
2020-08-30 22:04:38 -0500

    Actualizando el link de producción

--- env.js
+++ env.js
@@ -2,17 +2,17 @@ export const appConfig = {
     logo: 'https://imagenes.softaki.com/app/MMS/MMS-Logo-Fb.webp',
     logoEstatico: '/logo-mms-peru.png',
     nameToken: 'ktoken',
-    graphUrl: 'https://logica.mmsperusalud.com/public/graphql',
+    graphUrl: 'https://logica.cdstienda.com/public/graphql',
     facebookAppId: '729329960948099',
     googleClientId: '27183221542-cd37omof21mjjhrhl2dt6nigkdj8j7q7.apps.googleusercontent.com',
     redesSociales: {
-      facebook: '',
-      whatsapp: ''
+      facebook: 'https://www.facebook.com/BioySaludMedicinaAlternativa',
+      whatsapp: 'https://api.whatsapp.com/send?phone=51987648630&text=Buen%20d%C3%ADa!%20Requiero%20mas%20informaci%C3%B3n'
     },
     openGraph: {
       logo: 'https://imagenes.softaki.com/app/MMS/MMS-Logo-Fb.webp',
       whatsapp: 'https://imagenes.softaki.com/app/MMS/MMS-Logo-Wsp.webp',
-      urlWeb: 'https://tienda.mmmsperusalud.com',
+      urlWeb: 'https://www.cdstienda.com/',
       titulo: 'MMS Perú salud',
       descripcion: 'El MMS proviene de un mineral NaClo2 (Clorito de sodio) que al ser combinado con Acido cítrico libera un gas en el organismo (Dióxido de cloro) y al ser ingerido va al torrente sanguíneo y tiene la capacidad de eliminar de forma muy efectiva todas las bacterias, virus, hongos y parásitos.',
       keywords: 'MMS Peru Salud, MMS',
